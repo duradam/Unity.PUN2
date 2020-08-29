06b5833ed2af5acef8845a9a6e9d53cdecf7a0af
duradam <40144116+duradam@users.noreply.github.com>
2020-08-29 16:31:12 +0200

    continous script

---
+++ Assets/SetStartOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using static Photon.Pun.PhotonAnimatorView;
+
+public class SetStartOptions : MonoBehaviour
+{
+    PhotonAnimatorView PhotonAnimatorView;
+    void Start()
+    {
+        PhotonAnimatorView = GetComponent<PhotonAnimatorView>();
+        foreach (var par in PhotonAnimatorView.GetSynchronizedParameters())
+        {
+            par.SynchronizeType = SynchronizeType.Continuous;
+        }
+        foreach (var lay in PhotonAnimatorView.GetSynchronizedLayers())
+        {
+            lay.SynchronizeType = SynchronizeType.Continuous;
+        }
+    }
+
+ 
+}
