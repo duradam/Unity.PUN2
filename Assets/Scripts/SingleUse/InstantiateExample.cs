4bd3c8d4e49fe3a3019ea11cedc85528cc3207cd
duradam <40144116+duradam@users.noreply.github.com>
2020-08-29 16:13:06 +0200

    camera follows unka

--- Assets/Scripts/SingleUse/InstantiateExample.cs
+++ Assets/Scripts/SingleUse/InstantiateExample.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using MalbersAnimations;
 using UnityEngine;
 
 public class InstantiateExample : MonoBehaviour
 {
     [SerializeField]
     private GameObject _prefab;
+
+    public MFreeLookCamera FreeLookCamera; 
     private void Awake()
     {
-        transform.SetPositionAndRotation(new Vector3{x=transform.position.x-3, y=transform.position.y, z=transform.position.z}
+        Vector2 offset = Random.insideUnitCircle * 3f;
+        Vector3 position = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, transform.position.z);
+        transform.SetPositionAndRotation(new Vector3{x=transform.position.x, y=transform.position.y, z=transform.position.z}
             , Quaternion.identity) ;
-        MasterManager.NetworkInstantiate(_prefab, transform.position, Quaternion.identity);
+        var unka = MasterManager.NetworkInstantiate(_prefab, transform.position, Quaternion.identity);
+
+        FreeLookCamera.SetTarget(unka.transform); 
     }
 }
--- Assets/Scripts/Utilities/Network/QuickInstantiate.cs
+++ Assets/Scripts/Utilities/Network/QuickInstantiate.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using MalbersAnimations;
 using UnityEngine;
 
 public class QuickInstantiate : MonoBehaviour
@@ -7,9 +8,9 @@ public class QuickInstantiate : MonoBehaviour
     [SerializeField]
     private GameObject _prefab;
 
+
     private void Awake(){
         Vector2 offset = Random.insideUnitCircle * 3f;
         Vector3 position = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, transform.position.z);
-        MasterManager.NetworkInstantiate(_prefab, position, Quaternion.identity);  
     }
 }
